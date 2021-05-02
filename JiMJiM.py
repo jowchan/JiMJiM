e479c5155235e2689b20f01478bcc5870d914f64
jowchan <83477516+jowchan@users.noreply.github.com>
2021-05-02 12:31:41 -0700

    Add files via upload

--- JiMJiM.py
+++ JiMJiM.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 load_dotenv()
 import discord
 import os
-from discord.ext import commands
-import asyncio
+from discord.ext import commands, tasks
+import asyncio, datetime
 
 from discord import Member, Role
 
@@ -17,7 +17,10 @@ client = commands.Bot(command_prefix='$', intents=intents)
 
 # This is a GLOBAL variable for the syllabus command
 client.course_information= ""
-
+client.startdate = None
+client.enddate = None
+client.lectures = None
+client.lecture_starttime_dict = {}
 
 @client.event
 async def on_ready():
@@ -35,7 +38,7 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-
+    await create_rank_roles()
 
 @client.event
 async def on_guild_join(guild):
@@ -88,20 +91,54 @@ async def setup(ctx):
     msg = await client.wait_for("message", check=check)
     #global course_information
     if msg.content == "0":
-        embed1=discord.Embed(title="<a:pencil:838150754912436288> Please enter the link to your course information here:", color=0xffc0cb )
-        await ctx.send(embed = embed1)
-        client.course_information = await client.wait_for("message", check=check)
-        
+         embed1 = discord.Embed(
+            title="<a:pencil:838150754912436288> Please enter the link to your course information here:",
+            color=0xffc0cb)
+         await ctx.send(embed=embed1)
+         client.course_information = await client.wait_for("message", check=check)
+         embed2 = discord.Embed(title ="Course information has been saved. Enter $course_info command to access the link.",color=0xffc0cb )
+         await ctx.send(embed=embed2)
 
     
     elif msg.content == "1":
-        # ---------------------CHANGE THIS----------------------------------------------------
-        embed2=discord.Embed(title="<a:woman_teacher:838129346089582662> Please enter the number of instructors in this channel:", color=0xffc0cb )
-        await ctx.send(embed=embed2)
-        msg3 = await bot.wait_for("message", check=check)
-        #set number of instructor roles to user input heres
-        if(msg3.content =="3"): #this is for testing
-            await ctx.send("Three instructors total.")
+        embed3 = discord.Embed(
+            title="<a:woman_teacher:838129346089582662> Please enter course START DATE (format: mm/dd/yyyy)",
+            color=0xffc0cb)
+        await ctx.send(embed=embed3)
+        msg_start = await client.wait_for("message", check=check)
+        client.startdate = msg_start.content
+        print("start date: ", client.startdate)
+
+        embed3 = discord.Embed(
+            title="<a:woman_teacher:838129346089582662> Please enter course END DATE (format: mm/dd/yyyy)",
+            color=0xffc0cb)
+        await ctx.send(embed=embed3)
+        msg_end = await client.wait_for("message", check=check)
+        client.enddate = msg_end.content
+        print("end date: ", client.enddate)
+
+        embed3 = discord.Embed(
+            title="<a:woman_teacher:838129346089582662> Please enter LECTURE TIMES based on the following criteria---------:",
+            color=0xffc0cb)
+        embed3.add_field(
+            name="DayOfWeek StartTime - EndTime",
+            value="M/T/W/Th/F/Sat/Sun HH:MM - HH:MM",
+            inline=False)
+        embed3.add_field(
+            name="Example:",
+            value="M 10:30-11:00, Sun 00:30-18:00",
+            inline=False)
+        await ctx.send(embed=embed3)
+        msg_lectures = await client.wait_for("message", check=check)
+        client.lectures = msg_lectures.content
+        print("lectures: ", client.lectures)
+
+        embed3 = discord.Embed(
+            title="<a:woman_teacher:838129346089582662> Lecture reminders will be sent prior to every lecture. ",
+            color=0xffc0cb)
+        await ctx.send(embed=embed3)
+        # parse through user inputted lecture times and start the background task
+        await format_lecture_times(ctx)
         
             
     elif msg.content == "2":
@@ -111,11 +148,41 @@ async def setup(ctx):
 @client.command()
 async def course_info(ctx):
     if client.course_information == "":
-        await ctx.send("No course information has been provided.")
+        embed = discord.Embed(title ="No course information has been provided.",color=0xffc0cb )
+        
     else:  
         embed=discord.Embed(title="Course Information", url=client.course_information.content, color=0xffc0cb )
+    await ctx.send(embed=embed)
+
+@client.command()
+async def course_dates(ctx):
+    if client.startdate == "" or client.enddate == "":
+        await ctx.send("No course dates have been provided.")
+    else:
+        embed = discord.Embed(
+            title="Course Dates",
+            description=f"Start Date: {client.startdate} ~ End Date: {client.enddate}",
+            color=0xffc0cb)
         await ctx.send(embed=embed)
 
+@client.command()
+async def course_schedule(ctx):
+    if client.lectures == "":
+        await ctx.send("No course lecture times has been provided.")
+    else:
+        lecture_array = client.lectures.split(',')
+        embed = discord.Embed(
+            title="Course Lecture Times",
+            description="",
+            color=0xffc0cb)
+
+        for lecture_time in lecture_array:
+            embed.add_field(
+                name=f'{lecture_time}',
+                value="---",
+                inline=False)
+
+        await ctx.send(embed=embed)
 
 
 #------------- Role Functions ------------------------
@@ -128,7 +195,7 @@ async def create_role(ctx, name):
     await guild.create_role(name=name, mentionable=True, hoist=True)
     msg = discord.Embed(
         title=f'Role: {name} has been created',
-        color=0x67e0c4)
+        color=0xffc0cb)
     await ctx.send(embed=msg)
 
 
@@ -142,7 +209,7 @@ async def delete_role(ctx, name):
     await Role.delete(role)
     msg = discord.Embed(
         title=f'Role: {name} has been deleted',
-        color=0x67e0c4)
+        color=0xffc0cb)
     await ctx.send(embed=msg)
 
 
@@ -170,7 +237,7 @@ async def add_Instructor(ctx, *usernames):
         await member.edit(nick=member.name + " | Instructor 🎓")
         msg = discord.Embed(
             title=f'{user} has been assigned the role: Instructor',
-            color=0x67e0c4)
+            color=0xffc0cb)
         await ctx.send(embed=msg)
        
 
@@ -197,8 +264,24 @@ async def delete_user_role(ctx,role, *usernames):
         await member.edit(nick=member.name)
         msg = discord.Embed(
             title=f'{user} has been stripped of the role: {role}',
-            color=0x67e0c4)
+            color=0xffc0cb)
         await ctx.send(embed=msg)
+
+# on member join, assign Newbie role
+@client.event
+async def on_member_join(member):
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    role = discord.utils.get(guild.roles, name="Newbie")
+    await member.add_roles(role)
+    msg = discord.Embed(
+        title=f'Welcome {member.name} to server {guild}! \
+        You have been assigned the role: Newbie. To level up in rank, participate constructively in course Q&A to \
+        earn points.', color=0xffc0cb)
+
+    # Bot sends DM to member
+    await member.send(embed=msg)
 
 
 # ------------ Helper functions ------------------------------
@@ -231,5 +314,68 @@ async def create_rank_roles():
     await create_new_role("Newbie", 0xCC99C9)
  
     print("Rank Roles have been added to the Server")
+
+# ------------------Course Notifications------------------
+# set up lecture notifications
+async def format_lecture_times(ctx):
+    print("lecture times: ", client.lectures)
+    days_of_week = {
+        "M": 0,
+        "T": 1,
+        "W": 2,
+        "Th": 3,
+        "F": 4,
+        "Sat": 5,
+        "Sun": 6,
+    }
+    lecture_array = client.lectures.split(',')
+    for lecture in lecture_array:
+        split = lecture.strip().split(' ')
+        day = split[0]
+        starttime = split[1].split('-')[0]
+        client.lecture_starttime_dict[days_of_week[day]] = starttime
+    print(lecture_array)
+    print(client.lecture_starttime_dict)
+
+    # start infinite background notification task loop
+    lectureNotifications.start(ctx)
+
+
+@tasks.loop()
+async def lectureNotifications(ctx):
+
+    current_day = datetime.date.today().weekday()
+    current_hour = datetime.datetime.now().hour
+    current_minute = datetime.datetime.now().minute
+
+    if current_day in client.lecture_starttime_dict:
+        starthour = client.lecture_starttime_dict[current_day].split(":")[0]
+        startmins = client.lecture_starttime_dict[current_day].split(":")[1]
+        starting_datetime = datetime.datetime(2020, 1, 1, int(starthour), int(startmins), 0)
+        print("lecture start datetime: ", starting_datetime)
+
+        ten_minutes = datetime.timedelta(minutes=10)
+
+        # ten minutes earlier than starting_datetime (disregard the date, we just want the time)
+        final_datetime = starting_datetime - ten_minutes
+        print("notification datetime: ", final_datetime)
+        notification_hour = final_datetime.hour
+        notification_minute = final_datetime.minute
+
+        # print('notification hour', notification_hour)
+        # print('notification minute', notification_minute)
+        print(f'current time: {current_hour}:{current_minute}')
+
+        # if current time is between notification time & start of lecture
+        if current_hour == notification_hour \
+                and (notification_minute <= current_minute) \
+                and (abs(notification_minute - current_minute) <= 10):
+            reminder = discord.Embed(
+                title=f'<a:woman_teacher:838129346089582662> Lecture TODAY @ {client.lecture_starttime_dict[current_day]}',
+                color=0xffc0cb)
+            await ctx.send(embed=reminder)
+
+        await asyncio.sleep(60*3)
+
     
 client.run(TOKEN)
